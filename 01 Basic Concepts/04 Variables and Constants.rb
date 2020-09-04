ee29f5d4756089e536cdb8fbd8a32c4baa04ff74
Renzodef <renzodefrancesco@gmail.com>
2020-09-04 16:23:14 +0200

    4 September 2020

---
+++ 01 Basic Concepts/04 Variables and Constants.rb
@@ -0,0 +1,8 @@
+myNum = 31 # Variable
+MyNum = 42 # Constant
+
+# Trying to change the value produces a warning but will change it anyway
+MyNum = 8 # warning: already initialized constant MyNum 
+
+puts myNum
+puts MyNum
---
+++ 01 Basic Concepts/05 Data Types.rb
@@ -0,0 +1,15 @@
+# All variables in Ruby can be of all data types.
+# Ruby automatically determines data type by the value assigned to the variable.
+# For example:
+x = 42 # integer
+y = 1.58 # floating point value
+z = "Hello" # string
+
+# You can reassign a different value to a variable at any time.
+# To insert the value of a variable into a double quote string 
+# (a string is a sequence of characters, such as "Hello"), 
+# use the # symbol and curly brackets with the variable name.
+# For example:
+age = 42
+print "He is #{age} years old"
+# outputs "He is 42 years old"
